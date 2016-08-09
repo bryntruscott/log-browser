2e02426e0d4bfa05f76f45048e40b34df851fc95
Bryn Truscott <bryn.truscott@gmail.com>
2016-08-09 11:40:15 +0100

    Basic functionality

---
+++ log-browser/log.py
@@ -0,0 +1,18 @@
+import pymongo
+
+class Log(object):
+
+    def __init__(self, system, log_name):
+        self.__database = system
+        self.__collection_name = log_name
+        self.__collection = pymongo.MongoClient()[self.__database][self.__collection_name]
+
+    def get_last(self):
+        cursor = self.__collection.find().sort("time", pymongo.DESCENDING)
+        return cursor.next()
+
+class LogFactory(object):
+
+    @classmethod
+    def create_log(cls, system, log_name):
+        return Log(system, log_name)
